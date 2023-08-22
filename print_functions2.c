6dfc8a2437c84d2d23c7b14d0dbcc63e8caf59c2
dr3amy3gg <137756290+dr3amy3gg@users.noreply.github.com>
2023-08-22 12:07:03 +0200

    Update print_functions2.c

--- print_functions2.c
+++ print_functions2.c
@@ -4,23 +4,14 @@
 /****************** PRINT POINTER ******************/
 
 /**
-
  * print_pointer - Prints the value of a pointer variable
-
  * @types: List a of arguments
-
  * @buffer: Buffer array to handle print
-
  * @flags:  Calculates active flags
-
  * @width: get width
-
  * @precision: Precision specification
-
  * @size: Size specifier
-
  * Return: Number of chars printed.
-
  */
 
 int print_pointer(va_list types, char buffer[],
@@ -99,23 +90,14 @@ int print_pointer(va_list types, char buffer[],
 /************************* PRINT NON PRINTABLE *************************/
 
 /**
-
  * print_non_printable - Prints ascii codes in hexa of non printable chars
-
  * @types: Lista of arguments
-
  * @buffer: Buffer array to handle print
-
  * @flags:  Calculates active flags
-
  * @width: get width
-
  * @precision: Precision specification
-
  * @size: Size specifier
-
  * Return: Number of chars printed
-
  */
 
 int print_non_printable(va_list types, char buffer[],
@@ -172,23 +154,14 @@ int print_non_printable(va_list types, char buffer[],
 /************************* PRINT REVERSE *************************/
 
 /**
-
  * print_reverse - Prints reverse string.
-
  * @types: Lista of arguments
-
  * @buffer: Buffer array to handle print
-
  * @flags:  Calculates active flags
-
  * @width: get width
-
  * @precision: Precision specification
-
  * @size: Size specifier
-
  * Return: Numbers of chars printed
-
  */
 
 
@@ -251,23 +224,14 @@ int print_reverse(va_list types, char buffer[],
 /************************* PRINT A STRING IN ROT13 *************************/
 
 /**
-
  * print_rot13string - Print a string in rot13.
-
  * @types: Lista of arguments
-
  * @buffer: Buffer array to handle print
-
  * @flags:  Calculates active flags
-
  * @width: get width
-
  * @precision: Precision specification
-
  * @size: Size specifier
-
  * Return: Numbers of chars printed
-
  */
 
 int print_rot13string(va_list types, char buffer[],
