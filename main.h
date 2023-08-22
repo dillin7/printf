bbe0fbde87f372634b5e0e1d85f239c3fb62f2ca
dr3amy3gg <137756290+dr3amy3gg@users.noreply.github.com>
2023-08-22 12:04:34 +0200

    Update main.h

--- main.h
+++ main.h
@@ -35,15 +35,9 @@
 
 
 /**
-
  * struct fmt - Struct op
-
- *
-
  * @fmt: The format.
-
  * @fn: The function associated.
-
  */
 
 struct fmt
@@ -59,15 +53,9 @@ struct fmt
 
 
 /**
-
  * typedef struct fmt fmt_t - Struct op
-
- *
-
  * @fmt: The format.
-
  * @fm_t: The function associated.
-
  */
 
 typedef struct fmt fmt_t;
