0738336a986f771829d12e94fd296004cb2986d5
dr3amy3gg <137756290+dr3amy3gg@users.noreply.github.com>
2023-08-22 13:05:51 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -11,9 +11,7 @@ void print_buffer(char buffer[], int *buff_ind);
  */
 
 int _printf(const char *format, ...)
-
 {
-
         int i, printed = 0, printed_chars = 0;
 
         int flags, width, precision, size, buff_ind = 0;
@@ -24,37 +22,26 @@ int _printf(const char *format, ...)
 
 
         if (format == NULL)
-
                 return (-1);
-
 
         va_start(list, format);
 
-
         for (i = 0; format && format[i] != '\0'; i++)
-
         {
-
                 if (format[i] != '%')
-
                 {
-
                         buffer[buff_ind++] = format[i];
 
                         if (buff_ind == BUFF_SIZE)
-
                                 print_buffer(buffer, &buff_ind);
 
                         /* write(1, &format[i], 1);*/
 
                         printed_chars++;
-
                 }
 
                 else
-
                 {
-
                         print_buffer(buffer, &buff_ind);
 
                         flags = get_flags(format, &i);
@@ -72,24 +59,18 @@ int _printf(const char *format, ...)
                                 flags, width, precision, size);
 
                         if (printed == -1)
-
                                 return (-1);
 
                         printed_chars += printed;
-
                 }
-
         }
 
 
         print_buffer(buffer, &buff_ind);
 
-
         va_end(list);
 
-
         return (printed_chars);
-
 }
 
 
@@ -100,14 +81,9 @@ int _printf(const char *format, ...)
  */
 
 void print_buffer(char buffer[], int *buff_ind)
-
 {
-
         if (*buff_ind > 0)
-
                 write(1, &buffer[0], *buff_ind);
 
-
         *buff_ind = 0;
-
 }
